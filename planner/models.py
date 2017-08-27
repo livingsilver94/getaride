59593ef70650dbf5928073b1c1a4df6765753267
livingsilver94 <fabio94xx@gmail.com>
2017-08-27 15:39:49 +0200

    Fix Trip model

--- planner/models.py
+++ planner/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import models as auth_models
+from django.core.validators import MinValueValidator, ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 from cities_light.models import City
 
@@ -17,3 +19,10 @@ class Trip(models.Model):
     destination = models.ForeignKey(City, related_name='trip_destination')
     date_origin = models.DateTimeField(name='date_origin')
     passengers = models.ManyToManyField(PoolingUser)
+    price = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0)])
+
+    # Limit passenger number to 8
+    def clean(self, *args, **kwargs):
+        if self.passengers.count() > 8:
+            raise ValidationError(_("You are not allowed to drive with more than 8 passengers"))
+        super(Trip, self).clean(*args, **kwargs)
