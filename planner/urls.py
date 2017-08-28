6cc2ea4f9da59683b74527844fa99aea84e87534
livingsilver94 <fabio94xx@gmail.com>
2017-08-28 21:15:34 +0200

    Working login and logout urls

--- planner/urls.py
+++ planner/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.views import LogoutView, LoginView
 from .views import HomePageView, city_autocomplete, city_coordinates
+from .forms import LoginForm
 
 app_name = 'planner'
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='homepage'),
-    url(r'^login/', LoginView.as_view(template_name='planner/login_page.html'), name='login'),
+    url(r'^login/', LoginView.as_view(template_name='planner/login_page.html', authentication_form=LoginForm),
+        name='login'),
     url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^city-autocomplete/$', city_autocomplete, name="city-autocomplete"),
     url(r'^city-coordinates/$', city_coordinates, name='city-coordinates'),
