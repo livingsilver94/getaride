59bd1ad7f09c75ca55658d6290b2be4fc051f788
livingsilver94 <fabio94xx@gmail.com>
2017-08-28 10:58:30 +0200

    Move page elements to elements directory

--- planner/urls.py
+++ planner/urls.py
@@ -5,7 +5,7 @@ from .views import HomePageView, city_autocomplete, city_coordinates
 app_name = 'planner'
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='homepage'),
-    url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^login/', LoginView.as_view(template_name='planner/login_page.html'), name='login'),
     url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^city-autocomplete/$', city_autocomplete, name="city-autocomplete"),
     url(r'^city-coordinates/$', city_coordinates, name='city-coordinates'),
