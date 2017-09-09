26dfc03840bffd0b2a58beca221bf06285224227
Mithrandir94 <andrea.guzzetti94@gmail.com>
2017-09-09 23:33:28 +0200

    Fixed validate_adult method

--- planner/validators.py
+++ planner/validators.py
@@ -6,6 +6,6 @@ from django.utils.translation import ugettext_lazy as _
 def validate_adult(birth_date):
     today = datetime.date.today()
     age = today.year - birth_date.year
-    age -= (birth_date.day, birth_date.month) < (today.day, today.month)
+    age -= (birth_date.month, birth_date.day) < (today.month, today.day)
     if age < 18:
         raise ValidationError(_('You must be at least 18 to sign up'))
