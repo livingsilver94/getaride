fb2afca87a4139ee28a5cbfa0c7f7940bd5b5cdb
Mithrandir94 <andrea.guzzetti94@gmail.com>
2017-09-12 15:49:06 +0200

    Added arrival hour validator and corrected grammar in date validator in clean method ; added min_length in driving_license field on PoolingUser model; modified min value in total_price on Trip model and in price on Step model

--- planner/models.py
+++ planner/models.py
@@ -10,7 +10,7 @@ from datetime import date, timedelta
 
 class PoolingUser(models.Model):
     base_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    driving_license = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    driving_license = models.CharField(max_length=10, min_length=10, unique=True, blank=True, null=True)
     birth_date = models.DateField(blank=False, validators=[validate_adult],
                                   default=date.today() - timedelta(365.25 * 18) - timedelta(days=1))
 
@@ -23,7 +23,7 @@ class Trip(models.Model):
     destination = models.ForeignKey(City, related_name='trip_destination')
     date_origin = models.DateTimeField(name='date_origin')
     estimated_date_arrival = models.DateTimeField(name='est_date_arrival')
-    total_price = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0)])
+    total_price = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(1)])
 
 
 class Step(models.Model):
@@ -32,9 +32,9 @@ class Step(models.Model):
     hour_origin = models.TimeField()
     hour_destination = models.TimeField()
     passengers = models.ManyToManyField(PoolingUser)
-    price = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0)])
+    price = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0.01)])
     trip = models.ForeignKey(Trip, related_name='trip')
-    count = models.PositiveIntegerField(name='count')
+    count = models.PositiveIntegerField(name='count', validators=[MinValueValidator(0)])
 
     # Limit passenger number to 8
     def clean(self, *args, **kwargs):
@@ -43,6 +43,9 @@ class Step(models.Model):
             raise ValidationError(_("You are not allowed to drive with more than 8 passengers"))
 
         if self.estimated_date_arrival <= self.date_origin:
-            raise ValidationError(_("Estimated date arrival must be later then date origin"))
+            raise ValidationError(_("Estimated arrival date must be later than departure date"))
+
+        if self.hour_destination <= self.hour_origin:
+            raise ValidationError(_("Estimated arrival hour must be later than departure hour"))
 
         super(Trip, self).clean(*args, **kwargs)
