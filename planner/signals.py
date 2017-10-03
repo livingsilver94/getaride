8d7f3320a9d3fd3b7365cad7631835a0a46f374e
livingsilver94 <fabio94xx@gmail.com>
2017-10-03 12:19:07 +0200

    Make is_joinable automatic based of passenger number

--- planner/signals.py
+++ planner/signals.py
@@ -1,12 +1,13 @@
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 from .models import Step
 
 
 @receiver(m2m_changed, sender=Step.passengers.through)
 def check_passengers(sender, **kwargs):
     step = kwargs['instance']
-    if step.passengers.count() >= 8:
-        raise ValidationError(_("You exceeded passenger maximum number"))
+    if kwargs['action'] == 'post_add':
+        if step.passengers.count() >= step.trip.max_num_passengers:
+            step.trip.is_joinable = False
+    elif kwargs['action'] == 'post_remove':
+        step.trip.is_joinable = True
