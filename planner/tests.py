1d5348ea5c9aaf9e473311083a4a1461e20f1ddf
livingsilver94 <fabio94xx@gmail.com>
2017-10-03 14:46:26 +0200

    Add PoolingUser tests

--- planner/tests.py
+++ planner/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import PoolingUser
+from django.core.exceptions import ValidationError
+import datetime
+from .validators import validate_adult
 
-# Create your tests here.
+
+class PoolingUserTest(TestCase):
+    def test_is_not_driver(self):
+        usr = PoolingUser()
+        self.assertFalse(usr.is_driver())
+
+    def test_is_driver(self):
+        usr = PoolingUser(driving_license='1234567890')
+        self.assertTrue(usr.is_driver())
+
+    def test_is_not_adult(self):
+        self.assertRaises(ValidationError, lambda: validate_adult(datetime.datetime.today()))
+
+    def test_is_adult(self):
+        try:
+            validate_adult(datetime.datetime(1987, 1, 1))
+            success = True
+        except ValidationError as e:
+            self.fail(e.message)
+        else:
+            self.assertTrue(success)
