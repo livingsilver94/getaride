c060a2cb05d3e4990150ab30854c481a97a7bf5f
livingsilver94 <fabio94xx@gmail.com>
2017-10-04 14:16:52 +0200

    Add test for planner.models.Step.clean()

--- planner/tests.py
+++ planner/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-from .models import PoolingUser
+from .models import PoolingUser, Step
 from django.core.exceptions import ValidationError
 import datetime
 from .validators import validate_adult
+from cities_light.models import City
 
 
 class PoolingUserTest(TestCase):
@@ -17,3 +18,17 @@ class PoolingUserTest(TestCase):
     def test_adult_validator(self):
         self.assertRaises(ValidationError, lambda: validate_adult(datetime.datetime.today()))
         self.assertIsNone(validate_adult(datetime.datetime(1987, 1, 1)))
+
+
+class StepTest(TestCase):
+    def test_clean_method(self):
+        step = Step()
+        step.origin = City(id=0)
+        step.destination = City(id=0)
+        step.hour_origin = '10:00'
+        step.hour_destination = '10:00'
+        self.assertRaises(ValidationError, lambda: step.clean())
+        step.hour_destination = '11:00'
+        self.assertRaises(ValidationError, lambda: step.clean())
+        step.destination = City(id=1)
+        self.assertIsNone(step.clean())
