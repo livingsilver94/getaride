263ff63cda763729709a99256b8188c4c42ac729
livingsilver94 <fabio94xx@gmail.com>
2017-10-04 13:11:49 +0200

    Re-organize test names for readability

--- planner/tests.py
+++ planner/tests.py
@@ -6,22 +6,14 @@ from .validators import validate_adult
 
 
 class PoolingUserTest(TestCase):
-    def test_is_not_driver(self):
+    def not_a_driver_by_default(self):
         usr = PoolingUser()
         self.assertFalse(usr.is_driver())
 
-    def test_is_driver(self):
+    def is_driver_with_driving_license(self):
         usr = PoolingUser(driving_license='1234567890')
         self.assertTrue(usr.is_driver())
 
-    def test_is_not_adult(self):
+    def test_adult_validator(self):
         self.assertRaises(ValidationError, lambda: validate_adult(datetime.datetime.today()))
-
-    def test_is_adult(self):
-        try:
-            validate_adult(datetime.datetime(1987, 1, 1))
-            success = True
-        except ValidationError as e:
-            self.fail(e.message)
-        else:
-            self.assertTrue(success)
+        self.assertIsNone(validate_adult(datetime.datetime(1987, 1, 1)))
